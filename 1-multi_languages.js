3ec57d0a2e3de8111bf94f71a034d0b4d549615d
sideeqhammed <sideeqhammed05.com>
2025-07-10 23:24:56 +0100

    alx-projects

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,8 +1,13 @@
-const a = "C is fun"
-const b = "Python is cool"
-const c = "Javascript is amazing"
+// const a = "C is fun"
+// const b = "Python is cool"
+// const c = "Javascript is amazing"
 
 
-console.log(`${a}`);
-console.log(`${b}`);
-console.log(`${c}`);
+// console.log(`${a}`);
+// console.log(`${b}`);
+// console.log(`${c}`);
+
+
+message = "C is fun\nPython is cool\nJavascript is amazing";
+
+console.log(message);
